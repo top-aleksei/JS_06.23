b21e44e7eb6f84d9ade706222ea7e264d748b501
Aleksei <toporov.aleksei@gmail.com>
2023-05-31 18:09:25 +0300

    refactor: remove console logs

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 function getDistance(aX, aY, bX, bY) {
-  const args = [...arguments]
-if (args.length !== 4 || !args.every(el => Math.abs(el) <= 1000 && typeof el === 'number')) {
-  // throw new Error('oups')
-  console.log('oups: ');
-} else {
-  const res = ((bX - aX)**2 + (bY - aY)**2)**0.5.toFixed(2)
-  const trunkRes = Math.trunc(res * 100) / 100
-  console.log(res);
-  console.log(trunkRes);
-}
+  const args = [...arguments];
+  if (args.length !== 4 || !args.every((el) => Math.abs(el) <= 1000 && typeof el === 'number')) {
+    throw new Error();
+  } else {
+    const res = ((bX - aX) ** 2 + (bY - aY) ** 2) ** (0.5).toFixed(2);
+    const trunkRes = Math.trunc(res * 100) / 100;
+    return trunkRes;
+  }
 }
 
 function switchPlaces(arg) {
-if (!Array.isArray(arg)) {
-console.log('ups');
-} else if (arg.length === 0) {
-   console.log(arg);
-  return arg
-} else {
-  const middleIndex = Math.trunc(arg.length / 2)
-  if(arg.length % 2 === 0) {
-    console.log([...arg.slice(middleIndex, arg.length), ...arg.slice(0, middleIndex)]);
+  if (!Array.isArray(arg)) {
+    throw new Error();
   } else {
-    console.log([...arg.slice(middleIndex + 1, arg.length), arg[middleIndex], ...arg.slice(0, middleIndex)]);
+    const middleIndex = Math.trunc(arg.length / 2);
+    if (arg.length % 2 === 0) {
+      return [...arg.slice(middleIndex, arg.length), ...arg.slice(0, middleIndex)];
+    } else {
+      return [
+        ...arg.slice(middleIndex + 1, arg.length),
+        arg[middleIndex],
+        ...arg.slice(0, middleIndex),
+      ];
+    }
   }
-
-}
 }
 
-// getDistance(15, 8, -4.5, -9000)
-
-// switchPlaces([1, 2, 3, 4, 5])
-// switchPlaces([1, 2, 3, 4, 5, 6])
-// switchPlaces({})
-// switchPlaces([1, 3])
-
+function getDivisors(num) {
+  if (typeof num !== 'number' || isNaN(num) || !isFinite(num)) {
+    throw new Error();
+  } else {
+    const results = [];
+    for (let i = Math.abs(num); i > 0; i--) {
+      if (num % i === 0) {
+        results.push(i);
+      }
+    }
+    return results;
+  }
+}
