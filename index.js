442d3aa90a61795804106bb2923c1194bc507923
Aleksei <toporov.aleksei@gmail.com>
2023-06-06 17:47:39 +0300

    feat 1st task

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+// Task 1
+
 Array.prototype.customFilter = function (fn, context) {
-  console.log(this);
+  if (!fn || typeof fn !== 'function') {
+    throw new Error('Invalid argument.');
+  } else if ((context && typeof context !== 'object') || context === null) {
+    throw new Error('Invalid argument.');
+  } else {
+    const filteredArr = [];
+    for (let i = 0; i < this.length; i++) {
+      if (fn.call(context, this[i], i, this)) {
+        filteredArr.push(this[i]);
+      }
+    }
+    return filteredArr;
+  }
 };
-[2, 3].customFilter();
+
+// Task 2
